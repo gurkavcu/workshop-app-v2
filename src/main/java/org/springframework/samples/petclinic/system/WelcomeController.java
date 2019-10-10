8dbf41d1f52e811ce0d54fcdbd8920e3310619f3
Rommel Tinoco <rtinoco@zeppelinops.com>
2019-10-10 19:28:24 +0300

    update spring profile

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -14,12 +14,16 @@ class WelcomeController {
     @Autowired
     Environment env;
 
+
+    @Value("#{systemEnvironment['APP_NAMESPACE']?:'default'}")
+    private String APP_NAMESPACE;
+
     @GetMapping("/")
     public String welcome(ModelMap map) {
-        String environment = "default";
-        if(env.getActiveProfiles().length > 0){
+        String environment = APP_NAMESPACE;
+        /*if(env.getActiveProfiles().length > 0){
             environment = env.getActiveProfiles()[0];
-        }
+        }*/
         map.addAttribute("env", environment);
         return "welcome";
     }

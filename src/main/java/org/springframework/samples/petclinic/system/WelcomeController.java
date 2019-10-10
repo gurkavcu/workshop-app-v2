806f9b8b32c367954c365be358314f54e59f85b7
Rommel Tinoco <rtinoco@zeppelinops.com>
2019-10-10 19:49:00 +0300

    fix environment injection

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.system;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
